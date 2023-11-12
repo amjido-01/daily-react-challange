a7d58f6efed5529db8a5886c4d1749711a76afdf
alajido <96017433+Alajido@users.noreply.github.com>
2023-11-12 06:19:13 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+const initialItems = [
+  { id: 1, name: "orange" },
+  { id: 2, name: "mango" },
+  { id: 3, name: "tree" },
+];
+
+const [items, setItems] = useState(initialItems);
+const [filter, setFilter] = useState("");
+
+const handleSearch = (e) => {
+  const inputVal = e.target.value;
+  setFilter(inputVal)
+
+  const filterVals = initialItems.filter((item) => 
+  item.name.toLowerCase().includes(inputVal.toLowerCase())
+  )
+  setItems(filterVals)
+}
+  return (
+    <>
+      <p>hello search</p>
+      <input type="text" value={filter} onChange={handleSearch}/>
+
+      <ul>
+        {items.map((item) => {
+          return <li key={item.id}>{item.name}</li>
+        })}
+      </ul>
+    </>
+  );
+}
+
+export default App
