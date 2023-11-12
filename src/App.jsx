4486fdff86f6dae0f46b0e0007346bfeb2c8e636
alajido <96017433+Alajido@users.noreply.github.com>
2023-11-12 16:13:06 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -12,23 +12,24 @@ const [items, setItems] = useState(initialItems);
 const [filter, setFilter] = useState("");
 
 const handleSearch = (e) => {
-  const inputVal = e.target.value;
-  setFilter(inputVal)
+ const inputVal = e.target.value;
+ setFilter(inputVal);
 
-  const filterVals = initialItems.filter((item) => 
-  item.name.toLowerCase().includes(inputVal.toLowerCase())
-  )
-  setItems(filterVals)
+ const filterVals = initialItems.filter((item) => 
+ item.name.toLowerCase().includes(inputVal.toLowerCase())
+ )
+ setItems(filterVals)
 }
+
+
   return (
     <>
       <p>hello search</p>
       <input type="text" value={filter} onChange={handleSearch}/>
-
       <ul>
-        {items.map((item) => {
-          return <li key={item.id}>{item.name}</li>
-        })}
+       {items.map((item) => {
+        return <li key={item.id}>{item.name}</li>
+       })}
       </ul>
     </>
   );
