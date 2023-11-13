dd0bce5799c21c5deeee3aa000b5f8cf08a767be
alajido <96017433+Alajido@users.noreply.github.com>
2023-11-13 18:48:23 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,37 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-const initialItems = [
-  { id: 1, name: "orange" },
-  { id: 2, name: "mango" },
-  { id: 3, name: "tree" },
-];
+// const initialItems = [
+//   { id: 1, name: "orange" },
+//   { id: 2, name: "mango" },
+//   { id: 3, name: "tree" },
+// ];
 
-const [items, setItems] = useState(initialItems);
-const [filter, setFilter] = useState("");
+const [isOpen, setIsOpen] = useState(false);
 
-const handleSearch = (e) => {
- const inputVal = e.target.value;
- setFilter(inputVal);
-
- const filterVals = initialItems.filter((item) => 
- item.name.toLowerCase().includes(inputVal.toLowerCase())
- )
- setItems(filterVals)
+const handleToggler = () => {
+  setIsOpen(true)
 }
 
-
+const handleClosedToggler = () => {
+  setIsOpen(false)
+}
   return (
     <>
-      <p>hello search</p>
-      <input type="text" value={filter} onChange={handleSearch}/>
-      <ul>
-       {items.map((item) => {
-        return <li key={item.id}>{item.name}</li>
-       })}
-      </ul>
+      {isOpen && (
+        <div
+          style={{ width: "300px", height: "300px", border: "1px solid blue" }}
+        >
+          <p style={{ textAlign: "center", fontSize: "40px" }}>
+            hello from here
+          </p>
+        </div>
+      )}
+      <div style={{ display: "flex", gap: "10px" }}>
+        <button onClick={handleToggler}>open</button>
+        <button onClick={handleClosedToggler}>close</button>
+      </div>
     </>
   );
 }
