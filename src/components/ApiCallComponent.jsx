b5d7fb45682aeaea27fd417e07adb8ed1304fc52
alajido <96017433+Alajido@users.noreply.github.com>
2023-11-16 12:08:36 +0100

    working on the slider

--- src/components/ApiCallComponent.jsx
+++ src/components/ApiCallComponent.jsx
@@ -2,49 +2,55 @@ import { useState, useEffect } from "react";
 // https://jsonplaceholder.typicode.com/users
 export const ApiCallComponent = () => {
   const [data, setData] = useState([]);
-  const [initialData, setInitialData] = useState([])
+  const [initialData, setInitialData] = useState([]);
   const [filter, setFilter] = useState("");
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        const res = await fetch("https://jsonplaceholder.typicode.com/users");
 
-        if (!res.ok) {
-          throw new Error("something bad")
-        }
-
-        const result = await res.json()
-        setData(result)
-        setInitialData(result)
-        console.log(initialData);
-        console.log(data);
-      } catch (error) {
-        console.log(error);
+ useEffect(() => {
+  const getData = async() => {
+    try {
+      const res = await fetch("https://jsonplaceholder.typicode.com/users");
+      if (!res.ok) {
+        throw new Error("something bad happen")
       }
+
+      const result = await res.json()
+      setData(result);
+      setInitialData(result)
+    } catch(error) {
+      console.log(error);
     }
-    getData()
-  }, [])
-
-
-  const handleSearch = (e) => {
-    const inputVal = e.target.value;
-    setFilter(inputVal);
-    console.log(filter);
-
-    const filteredVal =  initialData.filter((item) => {
-      return item.name.toLowerCase().includes(inputVal.toLowerCase())
-    })
-    setData(filteredVal)
   }
+  getData()
+ }, [])
+
+ const handleFilter = (e) => {
+  const inputVal = e.target.value;
+  setFilter(inputVal)
+
+  const filterVal = initialData.filter((item) => 
+  item.name.toLowerCase().includes(inputVal.toLowerCase())
+  )
+
+  setData(filterVal)
+ }
 
   return (
     <div>
-      <input type="text" value={filter} onChange={handleSearch} placeholder="search..."/>
-      {data ? (<ul>
-        {data.map((user) => {
-          return <li key={user.id}>{user.name}</li>
-        })}
-      </ul>) : "loading..."}
+      <input
+        type="text"
+        value={filter}
+        onChange={handleFilter}
+        placeholder="search..."
+      />
+      {data ? (
+        <ul>
+          {data.map((user) => {
+            return <li key={user.id}>{user.name}</li>;
+          })}
+        </ul>
+      ) : (
+        "loading..."
+      )}
     </div>
-  )
+  );
 }
