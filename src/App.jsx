6456c5ac5f95777100328ac19ba1c737eb979093
alajido <96017433+Alajido@users.noreply.github.com>
2023-11-16 10:40:50 +0100

    working on form validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
-import { ToggleComponent } from './components/ToggleComponent';
-import { SearchFilterComponent } from './components/SearchFilterComponent';
+// import { ToggleComponent } from './components/ToggleComponent';
+// import { SearchFilterComponent } from './components/SearchFilterComponent';
 import { ApiCallComponent } from './components/ApiCallComponent';
 
 function App() {
--- src/components/ApiCallComponent.jsx
+++ src/components/ApiCallComponent.jsx
@@ -1,32 +1,50 @@
 import { useState, useEffect } from "react";
 // https://jsonplaceholder.typicode.com/users
-
 export const ApiCallComponent = () => {
-    const [data, setData] = useState([])
+  const [data, setData] = useState([]);
+  const [initialData, setInitialData] = useState([])
+  const [filter, setFilter] = useState("");
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const res = await fetch("https://jsonplaceholder.typicode.com/users");
 
-    useEffect(() => {
-        const getData = async () => {
-            try {
-                const res = await fetch(
-                  "https://jsonplaceholder.typicode.com/users");
-
-                  if (!res.ok) {
-                    console.log("holly smokes");
-                  }
-
-                  const result = await res.json()
-                  setData(result)
-            } catch(error) {
-                console.log(error);
-            }
+        if (!res.ok) {
+          throw new Error("something bad")
         }
-        getData()
-    }, [])
-  return <div>
-    {data ? (<ul>
+
+        const result = await res.json()
+        setData(result)
+        setInitialData(result)
+        console.log(initialData);
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getData()
+  }, [])
+
+
+  const handleSearch = (e) => {
+    const inputVal = e.target.value;
+    setFilter(inputVal);
+    console.log(filter);
+
+    const filteredVal =  initialData.filter((item) => {
+      return item.name.toLowerCase().includes(inputVal.toLowerCase())
+    })
+    setData(filteredVal)
+  }
+
+  return (
+    <div>
+      <input type="text" value={filter} onChange={handleSearch} placeholder="search..."/>
+      {data ? (<ul>
         {data.map((user) => {
-            return <li key={user.id}>{user.name}</li>
+          return <li key={user.id}>{user.name}</li>
         })}
-    </ul>) : "loading"}
-  </div>;
-};
+      </ul>) : "loading..."}
+    </div>
+  )
+}
